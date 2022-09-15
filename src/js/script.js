8555206021bf17c8d4f744a9c808f4b5772fa8a3
emman355 <earevalo355@gmail.com>
2022-09-15 15:10:30 +0800

    feature/#25-category-cards

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,43 @@ burgerMenu.addEventListener('click', function(){
 document.getElementById('copyright')
 .appendChild(document.createTextNode(new Date()
 .getFullYear()))
+
+// <!--################## Category section start ##################-->
+
+//category items
+// api url
+const api_url = "http://localhost:3000/src/js/data.json";
+// Defining async function
+async function getapi(url) {
+    // Storing response
+    const response = await fetch(url);
+    // Storing data in form of JSON
+    var data = await response.json();
+    show(data)
+}
+// Calling that async function
+getapi(api_url);
+
+//Return the elements in the categoryList array;
+function show(data) {
+  let cardContainer = document.querySelector(".card__container");
+  var categoryItem = []
+  categoryItem = data.categoryList;
+  //Map to the array of categoryItem
+  categoryItem.map(function (item, index) {
+    //Append to the card__container class
+    cardContainer.innerHTML += 
+    `<div class="card__item" key=${index}>
+      <div class="card_image">
+        <img src="/src/images/${item.image}" alt="category">
+      </div>
+      <div class="card__desc">
+        <p>${item.title}</p>
+        <p>${item.description}</p>
+      </div>
+    </div>`
+  });
+}
+
+// <!--################## Category section end ##################-->
+    
