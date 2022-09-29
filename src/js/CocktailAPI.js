9d36c87e681b92e8cdaaa2b9fa06ea40c42ce385
dove <ndoverte11@gmail.com>
2022-09-29 22:34:54 +0800

    #76 initialy stablished search box connection to API

--- src/js/CocktailAPI.js
+++ src/js/CocktailAPI.js
@@ -2,8 +2,22 @@ console.log(1, "First Load");
 
 class CockTailAPI {
   async getCategories(cat) {
-    const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + cat);
+    const response = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=" + cat
+    );
     var data = await response.json();
     return data;
+  }
+
+  async getSearchQuery(q) {
+    try {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${q}`
+      );
+      var data = await response.json();
+      return data;
+    } catch (err) {
+      throw new Error("The data faild to load", { cause: err });
+    }
   }
 }
